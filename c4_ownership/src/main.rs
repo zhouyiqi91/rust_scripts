9db709f27b6b62e29538d62c7bb427f4c8cf0857
zhouyiqi91 <386346852@qq.com>
2021-08-30 17:38:31 +0800

    s4_common

--- c4_ownership/src/main.rs
+++ c4_ownership/src/main.rs
@@ -1,3 +1,40 @@
 fn main() {
-    println!("Hello, world!");
+    let mut s = String::from("hello");
+    s.push_str(", world!"); // push_str() appends a literal to a String
+
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    println!("{} and {}", r1, r2);
+    let r3 = &mut s;
+    println!("{}", r3);
+
+    //println!("{} and {}", r1, r2); //problem 
+
+    let s: &str = "i am slice";
+    println!("{}", s);
+
+    let first = new_first(s);
+    println!("{}", first);
+}
+
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
+}
+
+fn new_first(s: &str) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+    return &s[..];
 }
