9655bc4eb2aa714f0314991363afd64bc9940a1d
zhouyiqi <zhouyiqi@singleronbio.com>
2021-09-06 14:53:31 +0800

    c9 done

--- c9_panic/src/main.rs
+++ c9_panic/src/main.rs
@@ -1,3 +1,19 @@
+use std::fs::File;
+use std::io::Write;
+
+
 fn main() {
-    println!("Hello, world!");
+    let f = File::open("hello.txt");
+    let f1 = match f {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            std::io::ErrorKind::NotFound => File::create("hello.txt").unwrap(),
+            _ => panic!("Problem opening the file: {:?}", error),
+        },
+    };
+
+    let mut fh = File::create("hello.txt").unwrap();
+    let s1: &str = "hello";
+    fh.write_all(s1.as_bytes()).unwrap();
+
 }
