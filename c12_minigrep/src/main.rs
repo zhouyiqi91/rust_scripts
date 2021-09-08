12948ffa97be523a427148b13bb707f2b7861486
zhouyiqi91 <386346852@qq.com>
2021-09-08 09:01:56 +0000

    c12_minigrep

--- c12_minigrep/src/main.rs
+++ c12_minigrep/src/main.rs
@@ -1,48 +1,20 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
+
+use c12_minigrep::Config;
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
-    };
+    });
 
-    run(config);
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-//https://stackoverflow.com/questions/65674207/why-does-a-function-accept-vecstring-when-its-parameter-is-of-type-string?noredirect=1&lq=1
-fn parse_config(args: &Vec<String>) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
-    Config { query, filename }
-}
-
-impl Config {
-    fn new(args: &Vec<String>) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-        Ok(Config { query, filename })
+    if let Err(e) = c12_minigrep::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
     }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!("With text:\n{}", contents);
-
-    OK(())
 }
