8ff527b55851a168c3e45a6dc1f2a49d9e4ce76c
zhouyiqi91 <386346852@qq.com>
2021-09-13 03:05:56 +0000

    c14 done

--- c14_cargo/src/lib.rs
+++ c14_cargo/src/lib.rs
@@ -4,14 +4,11 @@
 ///
 /// ```
 /// let arg = 5;
-/// let answer = my_crate::add_one(arg);
+/// let answer = c14_cargo::add_one(arg);
 ///
 /// assert_eq!(6, answer);
 /// ```
+
 pub fn add_one(x: i32) -> i32 {
     x + 1
-}
-
-fn main(){
-    
 }
